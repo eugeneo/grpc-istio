5721458a82f90678a159ce653e38504f6d2eb8fd
Eugene Ostroukhov <eostroukhov@google.com>
2023-02-18 00:38:22 +0000

    I think it is working.

--- src-client/client-main.cc
+++ src-client/client-main.cc
@@ -3,6 +3,7 @@
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
+#include "absl/strings/str_join.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 
@@ -16,10 +17,8 @@ using google::protobuf::TextFormat;
 const std::string kDefaultServiceAddress = "xds:///echo-backend-service:4004";
 
 absl::optional<std::string> GetCookie(FrontendResponse res) {
-  for (const auto &metadata : res.initial_metadata()) {
-    if (metadata.name() == "set-cookie") {
-      return metadata.value();
-    }
+  if (res.has_cookie()) {
+    return res.cookie();
   }
   return absl::nullopt;
 }
@@ -73,6 +72,59 @@ FindHostCookie(const std::map<std::string, std::string> &hosts_cookies,
   return absl::nullopt;
 }
 
+bool CheckStatus(grpc::Status status) {
+  if (status.ok()) {
+    return true;
+  }
+  std::cerr << absl::StrFormat("Error [%d] %s (%s)", status.error_code(),
+                               status.error_message(), status.error_details())
+            << std::endl;
+  return false;
+}
+
+std::map<std::string, std::pair<std::set<std::string>, size_t>>
+GetServerCallCounts(const FrontendResponses &res) {
+  std::map<std::string, std::pair<std::set<std::string>, size_t>> counts;
+  for (const auto &response : res.responses()) {
+    auto &r = response.response().response();
+    if (response.has_cookie()) {
+      counts[r.instance_id()].first.insert(response.cookie());
+    }
+    counts[r.instance_id()].second += 1;
+  }
+  return counts;
+}
+
+absl::optional<std::vector<std::pair<std::string, std::string>>>
+CallBackend(const std::unique_ptr<FrontendService::Stub> &client,
+            size_t requests_count, absl::optional<absl::string_view> cookie,
+            FrontendRequest_Method method) {
+  grpc::ClientContext ctx;
+  FrontendRequest req;
+  FrontendResponses res;
+  req.set_backend_url(kDefaultServiceAddress);
+  req.set_num_requests(requests_count);
+  req.set_method(method);
+  if (cookie.has_value()) {
+    auto header = req.add_metadata();
+    header->set_name("cookie");
+    header->set_value(std::string(*cookie));
+  }
+  if (!CheckStatus(client->CallBackend(&ctx, req, &res))) {
+    return absl::nullopt;
+  }
+  std::vector<std::pair<std::string, std::string>> result;
+  for (const auto &host_count : GetServerCallCounts(res)) {
+    std::cout << host_count.first << "\t" << host_count.second.second << "\t"
+              << absl::StrJoin(host_count.second.first, ", ") << std::endl;
+    std::string cookie =
+        host_count.second.first.empty() ? "" : *host_count.second.first.begin();
+    result.emplace_back(host_count.first, cookie);
+  }
+
+  return result;
+}
+
 int main(int argc, char *argv[]) {
   const char *address = "localhost:4004";
   if (argc > 1) {
@@ -82,43 +134,35 @@ int main(int argc, char *argv[]) {
   auto channel =
       grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
   auto client = FrontendService::NewStub(std::move(channel));
-  grpc::ClientContext ctx;
-  GetCookieRequest req;
-  GetCookieResponse res;
-  req.set_cluster_url(kDefaultServiceAddress);
-  req.set_host_substring("second");
-  req.set_max_requests(200);
-  auto status = client->GetCookies(&ctx, req, &res);
-  if (status.ok()) {
-    std::string message;
-    TextFormat::PrintToString(res, &message);
-    std::cout << message << std::endl;
-  } else {
-    std::cerr << absl::StrFormat("Error [%d] %s (%s)", status.error_code(),
-                                 status.error_message(), status.error_details())
-              << std::endl;
+  const size_t kReqs = 300;
+  std::cout << absl::StreamFormat(
+                   "Let's send %d requests spread over all backends", kReqs)
+            << std::endl;
+  auto backends =
+      CallBackend(client, kReqs, absl::nullopt, FrontendRequest_Method_echo1);
+  if (!backends.has_value() || backends->empty()) {
+    return 1;
   }
-  // auto cookies = GetCookies(client, kDefaultServiceAddress, 100, {});
-  // if (!cookies.ok()) {
-  //   std::cerr << cookies.status() << std::endl;
-  //   return 1;
-  // }
-  // auto cookie = FindHostCookie(*cookies, "g2");
-  // if (!cookie.has_value()) {
-  //   std::cerr << "Cookie for a host was not found" << cookie.has_value()
-  //             << std::endl;
-  //   return 1;
-  // }
-  // std::cout << *cookie << std::endl;
-  // auto status =
-  //     GetCookies(client, kDefaultServiceAddress, 100, {{"cookie", *cookie}});
-  // if (!cookies.ok()) {
-  //   std::cerr << cookies.status() << std::endl;
-  //   return 1;
-  // }
-  // for (const auto &host_header : *status) {
-  //   std::cout << "!" << host_header.first << ": " << host_header.second
-  //             << std::endl;
-  // }
+  const auto &host_cookie = backends->at(backends->size() / 2);
+  std::cout << std::endl;
+  std::cout << absl::StreamFormat(
+                   "Let's send %d requests to %s backend (Echo method)", kReqs,
+                   host_cookie.first)
+            << std::endl;
+  if (!CallBackend(client, kReqs, host_cookie.second,
+                   FrontendRequest_Method_echo1)
+           .has_value()) {
+    return 1;
+  }
+  std::cout << std::endl;
+  std::cout << absl::StreamFormat(
+                   "Let's send %d requests to %s backend (Echo2 method)", kReqs,
+                   host_cookie.first)
+            << std::endl;
+  if (!CallBackend(client, kReqs, host_cookie.second,
+                   FrontendRequest_Method_echo2)
+           .has_value()) {
+    return 1;
+  }
   return 0;
 }
--- src-server/server_main.cc
+++ src-server/server_main.cc
@@ -18,20 +18,6 @@ ABSL_FLAG(std::string, bind_address, "0.0.0.0:4004", "Address to listen on");
 ABSL_FLAG(bool, frontend, false,
           "Specify backend URL if this is a t1 instance");
 
-grpc::Status FillResponse(
-    GetCookieResponse *response,
-    const std::map<std::string, std::pair<std::string, size_t>> &other_hosts,
-    int requests) {
-  for (const auto &host_data : other_hosts) {
-    auto *host = response->add_other_hosts();
-    host->set_host(host_data.first);
-    host->set_cookie(host_data.second.first);
-    host->set_count(host_data.second.second);
-  }
-  response->set_requests(requests);
-  return grpc::Status::OK;
-}
-
 class FrontendServiceImpl final : public FrontendService::Service {
 public:
   explicit FrontendServiceImpl(absl::string_view id) : id_(id) {}
@@ -56,22 +42,16 @@ private:
         const auto &metadata = request->metadata(i);
         ctx.AddMetadata(metadata.name(), metadata.value());
       }
-      auto status = backend->SendEcho(&ctx, backend_req, &backend_res);
+      auto status = request->method() == FrontendRequest_Method_echo1
+                        ? backend->SendEcho(&ctx, backend_req, &backend_res)
+                        : backend->SendEcho2(&ctx, backend_req, &backend_res);
       auto response = responses->add_responses();
       response->set_peer(context->peer());
-      for (const auto &name_value : ctx.GetServerInitialMetadata()) {
-        auto metadata = response->add_initial_metadata();
-        metadata->set_name(
-            std::string(name_value.first.data(), name_value.first.length()));
-        metadata->set_value(
-            std::string(name_value.second.data(), name_value.second.length()));
-      }
-      for (const auto &name_value : ctx.GetServerTrailingMetadata()) {
-        auto metadata = response->add_trailing_metadata();
-        metadata->set_name(
-            std::string(name_value.first.data(), name_value.first.length()));
-        metadata->set_value(
-            std::string(name_value.second.data(), name_value.second.length()));
+      const auto &initial_metadata = ctx.GetServerInitialMetadata();
+      auto it = initial_metadata.find("set-cookie");
+      if (it != initial_metadata.end()) {
+        const auto &cookie = it->second;
+        response->set_cookie(std::string(cookie.data(), cookie.size()));
       }
       if (!status.ok()) {
         auto error = response->mutable_response()->mutable_error();
@@ -83,51 +63,6 @@ private:
       }
     }
     return grpc::Status::OK;
-  }
-
-  grpc::Status GetCookies(grpc::ServerContext *context,
-                          const GetCookieRequest *request,
-                          GetCookieResponse *response) {
-    auto channel = grpc::CreateChannel(request->cluster_url(),
-                                       grpc::InsecureChannelCredentials());
-    auto stub = EchoService::NewStub(channel);
-    std::map<std::string, std::pair<std::string, size_t>> hosts;
-    for (int i = 0; i < request->max_requests(); ++i) {
-      grpc::ClientContext ctx;
-      EchoRequest req;
-      req.set_query(absl::StrFormat("Request #%d to url %s", i + 1,
-                                    request->cluster_url()));
-      EchoResponse res;
-      auto status = stub->SendEcho(&ctx, req, &res);
-      if (!status.ok()) {
-        auto error = response->mutable_error();
-        error->set_code(status.error_code());
-        error->set_message(status.error_message());
-        error->set_details(status.error_details());
-        return FillResponse(response, hosts, i);
-      } else {
-        const auto &instance_id = res.instance_id();
-        const auto &metadata = ctx.GetServerInitialMetadata();
-        auto it = metadata.find("set-cookie");
-        absl::optional<std::string> cookie;
-        if (it != metadata.end()) {
-          cookie = std::string(it->second.data(), it->second.size());
-        }
-        if (instance_id.find(request->host_substring()) != std::string::npos) {
-          response->set_host(instance_id);
-          if (cookie.has_value()) {
-            response->set_cookie(*cookie);
-          } else {
-            std::cerr << "No cookie for host " << instance_id << std::endl;
-          }
-          return FillResponse(response, hosts, i);
-        } else {
-          hosts[instance_id].first = cookie.has_value() ? *cookie : "<missing>";
-          hosts[instance_id].second += 1;
-        }
-      }
-    }
-    return FillResponse(response, hosts, request->max_requests());
   }
 
   const std::string id_;
@@ -146,8 +81,16 @@ private:
               << context->peer() << std::endl;
     response->set_instance_id(id_);
     response->set_query(request->query());
-    context->AddInitialMetadata("initial-metadata-host-id", "im " + id_);
-    context->AddTrailingMetadata("trailing-metadata-host-id", "tm " + id_);
+    return grpc::Status::OK;
+  }
+
+  grpc::Status SendEcho2(grpc::ServerContext *context,
+                         const EchoRequest *request,
+                         EchoResponse *response) override {
+    std::cout << "Request: \"" << request->query() << "\" from "
+              << context->peer() << std::endl;
+    response->set_instance_id(id_);
+    response->set_query(request->query());
     return grpc::Status::OK;
   }
 
